2edc6620d26de3a6e5a8447da41f889389c546e8
sherlockandjhp <983942147@qq.com>
2025-07-06 08:20:43 +0800

    修复模板路径和URL配置

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+# 必须添加这个index视图函数
+def index(request):
+    return render(request,'./blog/post_list.html')
+    
+    # 或者如果你想返回模板：
+    # return render(request, 'blog/index.html')
 # Create your views here.
