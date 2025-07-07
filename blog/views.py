99f723bf7e801f7971e822f70c3296406c65de6d
sherlockandjhp <983942147@qq.com>
2025-07-07 08:38:56 +0800

    更新了几篇文章

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post 
+from django.utils import timezone
 # 必须添加这个index视图函数
 def index(request):
-    return render(request,'./blog/post_list.html')
+    posts=Post.objects.all().order_by('-published_date')
+    return render(request,'./blog/post_list.html',{'posts':posts})
     
     # 或者如果你想返回模板：
     # return render(request, 'blog/index.html')
